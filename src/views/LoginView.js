eccec1004885c6452519f2a74190714ce967609a
alexbuchan <alex.buchan@telefonica.net>
2020-03-12 16:29:39 +0100

    Create LoginView and SignupView views

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import UserActions from '../actions/user';
-import UserStore from '../stores/user';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
 import SignupLoginBackground from '../components/SignupLoginBackground';
 import UserLogin from '../components/UserLogin';
 
 class LoginView extends React.Component {
-  loginUser() {
-    console.log('Login');
+  loginUser(user) {
+    UserActions.loginUser(user);
   }
 
   render() {
     return (
       <div className="row">
         <SignupLoginBackground />
-        <UserLogin loginUser={ this.loginUser.bind(this) } />
+        <UserLogin auth={ this.props.auth } loginUser={ this.loginUser.bind(this) } />
       </div>
     );
   }
--- src/views/SignupView.js
+++ src/views/SignupView.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import UserActions from '../actions/user';
-import UserStore from '../stores/user';
 import SignupLoginBackground from '../components/SignupLoginBackground';
 import UserSignup from '../components/UserSignup';
 
 class SignupView extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      user: {}
-    }
-
-    this._onChange = this._onChange.bind(this);
-  }
-
-  _onChange() {
-    this.setState({ user: UserStore._getUser() });
-  }
-
-  componentDidMount() {
-    UserStore.addChangeListener(this._onChange);
-  }
-
-  componentWillUnmount() {
-    UserStore.removeChangeListener(this._onChange);
-  }
-
   signupUser(user) {
-    UserActions.createUser(user);
+    UserActions.registerUser(user);
   }
 
   render() {
     return (
       <div className="row">
         <SignupLoginBackground />
-        <UserSignup signupUser={ this.signupUser.bind(this) } />
+        <UserSignup auth={ this.props.auth } signupUser={ this.signupUser.bind(this) } />
       </div>
     );
   }
