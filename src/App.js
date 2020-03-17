2868aa098cb90b1cb18d048977880b2265123555
alexbuchan <alex.buchan@telefonica.net>
2020-03-17 14:36:01 +0100

    Add Cookie to App state. Add flash message component to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* REACT IMPORTS */
+/* LIBRARY IMPORTS */
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
@@ -6,29 +6,43 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 /* STORE IMPORTS */
 import UserStore from './stores/user';
 
-/* COMPONENT IMPORTS */
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+/* ACTION IMPORTS */
+import UserActions from './actions/user';
+
+/* VIEW IMPORTS */
 import SignupView from './views/SignupView';
 import LoginView from './views/LoginView';
 import Contacts from './views/ContactsView';
 
+/* COMPONENT IMPORTS */
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import GenericNotFound from './components/GenericNotFound/GenericNotFound';
+import FlashMessage from './components/FlashMessage/FlashMessage';
+
 /* STYLES IMPORTS */
-import './styles.scss';
+import './styles/styles.scss';
 
 /* ROOT COMPONENT */
 class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      userStore: UserStore.getUserState()
+      userStore: UserStore.getUserState(),
+      closeFlashMessage: UserStore.getCloseFlashMessage(),
+      error: ''
     }
 
     this._onChange = this._onChange.bind(this);
   }
 
   _onChange() {
-    this.setState({ userStore: UserStore.getUserState() });
+    this.setState({
+      userStore: UserStore.getUserState(),
+      error: UserStore.getError(),
+      closeFlashMessage: UserStore.getCloseFlashMessage()
+    });
   }
 
   componentDidMount() {
@@ -40,9 +54,21 @@ class App extends React.Component {
   }
 
   isLoggedIn = () => {
-    if (this.state.userStore.auth) return <Contacts />;
+    if (this.state.userStore.cookie) return <Contacts />;
 
     return <Redirect to='/login' />;
+  }
+
+  renderFlashMessage() {
+    return (
+        <FlashMessage
+          message={ this.state.error }
+          type='error'
+          close={ this.state.closeFlashMessage }
+          closeFlashMessage={ UserActions.closeFlashMessage }
+          timeout={ 3000 }
+        />
+    );
   }
 
   render() {
@@ -50,16 +76,17 @@ class App extends React.Component {
       <Router>
         <div className='container-fluid'>
           <NavBar 
-            isLoggedIn={ this.state.userStore.auth }
-            userName={ (this.state.userStore.user) ? this.state.userStore.user.name : null }
+            cookie={ this.state.userStore.cookie }
           />
 
           <Switch>
-            <Route exact path="/" render={ () => <SignupView auth={ this.state.userStore.auth } /> } />
-            <Route exact path="/login" render={ () => <LoginView auth={ this.state.userStore.auth } /> } />
+            <Route exact path="/" render={ () => <SignupView cookie={ this.state.userStore.cookie } /> } />
+            <Route exact path="/login" render={ () => <LoginView cookie={ this.state.userStore.cookie } /> } />
             <Route exact path="/contacts" render={ this.isLoggedIn } />
+            <Route path='/*' component={ GenericNotFound } />
           </Switch>
 
+          { this.renderFlashMessage() }
           <Footer />
         </div>
       </Router>
