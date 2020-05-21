bbe355505f1aed36082686cf046d1f82d0d2e683
alexbuchan <alex.buchan@telefonica.net>
2020-05-21 09:52:46 +0200

    Add App folder in /src. Add App unit tests

--- src/App/App.js
+++ src/App/App.js
@@ -3,26 +3,26 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /* ACTION IMPORTS */
-import UserActions from './actions/user/UserActions';
+import UserActions from '../actions/user/UserActions';
 
 /* VIEW IMPORTS */
-import Contacts from './views/Contacts/ContactsView';
-import Settings from './views/Settings';
-import GenericNotFound from './views/GenericNotFoundPage/GenericNotFound';
+import Contacts from '../views/Contacts/ContactsView';
+import Settings from '../views/Settings/Settings';
+import GenericNotFound from '../views/GenericNotFoundPage/GenericNotFound';
 
 /* COMPONENT IMPORTS */
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
-import FlashMessage from './components/FlashMessage/FlashMessage';
-import SignupForm from './components/SignupForm/SignupForm';
-import LoginForm from "./components/LoginForm/LoginForm";
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/Footer';
+import FlashMessage from '../components/FlashMessage/FlashMessage';
+import SignupForm from '../components/SignupForm/SignupForm';
+import LoginForm from "../components/LoginForm/LoginForm";
 
 /* HOC IMPORTS */
-import withUserEntry from './HOC/UserEntryView/UserEntryView';
-import withAuthentication from './HOC/Authentication/Authentication';
+import withUserEntry from '../HOC/UserEntryView/UserEntryView';
+import withAuthentication from '../HOC/Authentication/Authentication';
 
 /* STYLES IMPORTS */
-import './styles/styles.scss';
+import '../styles/styles.scss';
 
 /* ROOT COMPONENT */
 class App extends React.Component {
