8ffa5177f49c1117afe2355b42c9d013faf7b375
alexbuchan <alex.buchan@telefonica.net>
2020-05-29 11:38:25 +0200

    Fix bug where empty fields being validated would never display is empty message

--- src/lib/Validator/validation_methods.js
+++ src/lib/Validator/validation_methods.js
@@ -3,14 +3,14 @@ export const isEmpty = (field) => {
 };
 
 export const usernameLength = (username) => {
-  return (username.length > 2);
+  return (username.length > 2 || isEmpty(username));
 };
 
 export const isValidEmail = (email) => {
   const regex = RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
-  return regex.test(email);
+  return (regex.test(email) || isEmpty(email));
 };
 
 export const passwordLength = (password) => {
-  return (password.length > 6);
+  return (password.length > 6 || isEmpty(password));
 };
